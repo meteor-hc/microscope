0fef21693476a49ca9f7f60600d47f625518a1bd
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-11 23:03:45 +0100

    Add 404

--- client/routes.js
+++ client/routes.js
@@ -1,9 +1,15 @@
 FlowRouter.route('/', {
     action: () =>
-        BlazeLayout.render("layout", {content: "postsList"})
+        BlazeLayout.render("layout", {content: 'postsList'})
 })
 
 FlowRouter.route('/post/:_id', {
     action: () =>
-        BlazeLayout.render("layout", {content: "postPage"})
+        BlazeLayout.render("layout", {content: 'postPage'})
 })
+
+FlowRouter.notFound = {
+    action: function() {
+        BlazeLayout.render("layout", {content: 'notFound'})
+    }
+}
