8ab658a79396d1f72e758ecade364da1c168cb22
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-11 14:41:19 +0100

    Add fixtures

---
+++ server/fixtures.js
@@ -0,0 +1,10 @@
+if (Posts.find().count() === 0) {
+    Posts.insert(
+        { "title" : "Doc Meteor", "url" : "https://github.com/meteor/docs" })
+    Posts.insert(
+        { "title" : "Guide Blaze", "url" : "http://blazejs.org" })
+    Posts.insert(
+        { "title" : "Doc Meteor", "url" : "https://github.com/meteor/docs" })
+    Posts.insert(
+        { "title" : "scopyleft", "url" : "http://scopyleft.fr" })
+}
