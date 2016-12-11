795a34f00b40cdfbf128144b2dc75c4ce6333ba5
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-11 23:04:32 +0100

    Add link to discuss

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -3,5 +3,12 @@ Template.postItem.helpers({
         const a = document.createElement('a')
         a.href = this.url
         return a.hostname
+    },
+    pathForPost: function() {
+        const params = { _id: this._id}
+        const routeName = 'postList'
+        console.log(params)
+        // FlowRouter.path(routeName, params, {})
+        return `/post/${params._id}`
     }
 })
