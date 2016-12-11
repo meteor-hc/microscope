c4d0657fdebb94b2dd9dd3805ce203595bef3f85
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-11 14:39:02 +0100

    Remove static data

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,18 +1,4 @@
-const postsData = [
-    {
-        title: 'Guide Meteor',
-        url: 'https://guide.meteor.com'
-    },
-    {
-        title: 'Guide Blaze',
-        url: 'http://blazejs.org'
-    },
-    {
-        title: 'Doc Meteor',
-        url: 'https://github.com/meteor/docs'
-    }
-]
 
 Template.postsList.helpers({
-    posts: postsData
+    posts: Posts.find()
 })
