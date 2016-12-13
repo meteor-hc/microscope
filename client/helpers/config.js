6d89e2aa5dd8478079c7b94b86020dfa988da1ef
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-13 18:04:42 +0100

    Reactive log

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,3 +1,27 @@
 Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_ONLY'
+    passwordSignupFields: 'USERNAME_ONLY'
 })
+
+Posts.find().observe({
+    added: function(post) {
+    // quand le callback 'added' est déclenché, ajout de l'élément HTML
+    // $('ul').append('<li id="' + post._id + '">' + post.title + '</li>')
+    console.log("post added")
+    },
+    changed: function(post) {
+    // quand le callback 'changed' est déclenché, modification du texte de l'élément HTML
+    // $('ul li#' + post._id).text(post.title)
+    console.log("post changed")
+    },
+    removed: function(post) {
+    // quand le callback 'removed' est déclenché, suppression de l'élément HTML
+    // $('ul li#' + post._id).remove()
+    console.log("post removed")
+    }
+})
+
+Meteor.startup(() =>
+    Tracker.autorun(() =>
+        console.log('There are ' + Posts.find().count() + ' posts')
+    )
+)
