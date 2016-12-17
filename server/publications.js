ded2e5b6eeed2faa2c0f8acaee78a287619bc037
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-17 21:21:30 +0100

    Uncomment checking

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,6 @@
 Meteor.publish('posts', () => Posts.find())
 Meteor.publish('singlePost', id => {
-  // check(id, String)
+  check(id, String)
   Meteor._sleepForMs(1000)
   return Posts.find({_id: id})
 })
