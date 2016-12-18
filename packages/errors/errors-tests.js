002c2b0f5ae1345c68625a15586ff877cb4f3a7d
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-18 15:03:44 +0100

    Remove mongo-livedata

--- packages/errors/errors-tests.js
+++ packages/errors/errors-tests.js
@@ -4,8 +4,19 @@ import { Tinytest } from "meteor/tinytest";
 // Import and rename a variable exported by errors.js.
 import { name as packageName } from "meteor/pntbr:errors";
 
-// Write your tests here!
-// Here is an example.
-Tinytest.add('errors - example', function (test) {
-  test.equal(packageName, "errors");
-});
+Tinytest.add("Errors - collection", test => {
+    test.equal(Errors.collection.find({}).count(), 0)
+    Errors.throw('A new error!')
+    test.equal(Errors.collection.find({}).count(), 1)
+    Errors.collection.remove({})
+})
+
+Tinytest.addAsync("Errors - template", (test, done) => {
+    Errors.throw('A new error!')
+    test.equal(Errors.collection.find({}).count(), 1)
+    UI.insert(UI.render(Template.meteorErrors), document.body)
+    Meteor.setTimeout(() => {
+        test.equal(Errors.collection.find({}).count(), 0)
+        done()
+    }, 3500)
+})
--- packages/errors/package.js
+++ packages/errors/package.js
@@ -1,26 +1,24 @@
 Package.describe({
-  name: 'pntbr:errors',
-  version: '0.0.1',
-  summary: 'A pattern to display application errors to the user',
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'pntbr:errors',
+    version: '0.0.1',
+    summary: 'A pattern to display application errors to the user',
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: null
 })
 
 Package.onUse(api => {
-  api.versionsFrom('1.4.2.3')
-  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client')
-  api.use('mongo', ['client', 'server'])
-  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client')
-  api.use('ecmascript')
-  api.mainModule('errors.js')
-  api.export('Errors')
+    api.versionsFrom('1.4.2.3')
+    api.use(['minimongo', 'templating@1.2.15'], 'client')
+    api.use('mongo', ['client', 'server'])
+    api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client')
+    api.use('ecmascript')
+    api.mainModule('errors.js')
+    api.export('Errors')
 })
 
 Package.onTest(api => {
-  api.use('ecmascript')
-  api.use('tinytest')
-  api.use('pntbr:errors')
-  api.mainModule('errors-tests.js')
+    api.use(['ecmascript', 'tinytest', 'pntbr:errors', 'templating@1.2.15', 'test-helpers'], 'client')
+    api.mainModule('errors-tests.js', 'client')
 })
